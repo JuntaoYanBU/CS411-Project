22be7e82be1d704b981f8c9f432f4f63163f5660
William Yang <wyang25@bu.edu>
2024-03-03 20:18:21 -0500

    test.py ver. ChatGPT
    Copied code from ChatGPT for analyzing how to connect flask to front end

--- test.py
+++ test.py
@@ -1,8 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
-
+# Define routes
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
+
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    # Example data to send to the front end
+    data = {'message': 'Hello from Flask!'}
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
