48d4a539d3a595cac7a97b363c1b8aa108bcadc9
wyang25 <wyang25@bu.edu>
2024-03-05 21:01:25 -0500

    Added MongoDB connection with help of ChatGPT

--- test.py
+++ test.py
@@ -1,5 +1,16 @@
 from flask import Flask, render_template, request, jsonify
+from pymongo import MongoClient
 
+# Accesses William Yang's MongoDB client...
+client = MongoClient("mongodb+srv://wyang25:<password>@cluster0.l0ejen6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+# ...Finds the database named Cluster0 in my account...
+db = client['Cluster0']
+# ...and pulls up a collection of data (in this case, the neighborhoods of sample restaurants)
+collection = db['sample_restaurants.neighborhoods']
+# Then, we decide on something that we want (a neighborhood in this case)...
+query = {"name": "Bedford"}
+# ... search for it in the collection.
+results = collection.find(query)
 app = Flask(__name__)
 
 # Define routes
@@ -9,8 +20,9 @@ def index():
 
 @app.route('/api/data', methods=['GET'])
 def get_data():
-    # Example data to send to the front end
-    data = {'message': 'Hello from Flask!'}
+    # Attempting to send data to front end. Warning! Does not work,
+    # Even when password is inputted into the url correctly!
+    data = results
     return jsonify(data)
 
 if __name__ == '__main__':
